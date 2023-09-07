5d40155cc5348a4f89d07268561edb7833d6415b
Martin Koldodovsky <mkoldov@gmail.com>
2023-09-07 15:13:11 +0200

    109

--- pub/demo1/js/js_interactions.js
+++ pub/demo1/js/js_interactions.js
@@ -1,24 +1,93 @@
 ﻿$(document).ready(function () {
     var params = { "callback": "onContext" };
-    smxProxy.sendRequest("get-context", null, "onContext");
+    try {
+        smxProxy.sendRequest("get-context", null, "onContext");
+    } catch (err) {
+        console.error(err)
+    }
 });
 
 function onContext(context) {
     const tree = JSONTree.create(context);
     $("#example").html(tree);
     log(`\n onContext ${JSON.stringify(context, null, 2)}`);
+
+    if (context && context.event) {
+        const rulebaseContext = getRulebaseContext(context.event);
+        console.log(rulebaseContext);
+
+        if (rulebaseContext) {
+            showAccessRulebase(rulebaseContext.accessPolicy, rulebaseContext.accessLayer);
+        }
+    }
 }
 
 function log(txt) {
-    document.getElementById("log").innerText = 
-    document.getElementById("log").innerText + txt;
+    document.getElementById("log").innerText =
+        document.getElementById("log").innerText + txt;
+}
+
+function getRulebaseContext(event) {
+    let accessLayer = null;
+    let accessPolicy = null;
+    if (event) {
+        if (event["trigger-id"]) {
+            if (event["trigger-id"] === "details-pane") {
+                if (event["objects"]) {
+                    console.log(event["objects"].name, event["objects"].type)
+                    for (let object of event["objects"]) {
+                        if (object["type"] === "access-layer") {
+                            accessLayer = object.uid;
+                        }
+                        if (object["type"] === "AccessPolicy") {
+                            accessPolicy = object.name;
+                        }
+                    }
+                    if (accessLayer && accessPolicy) {
+                        log(`\n\n accessLayer: ${accessLayer} accessPolicy: ${accessPolicy}`)
+                        return { "accessLayer": accessLayer, "accessPolicy": accessPolicy };
+                    }
+                }
+            }
+        }
+    }
+    return null;
 }
 
 function handleClick() {
-    alert('hello');
-    smxProxy.sendRequest("run-readonly-command",
-        { "command": "show gateways-and-servers", "parameters": { "details-level": "full" } },
-        "onReadOnlyCommand");
+
+    try {
+        smxProxy.sendRequest("run-readonly-command",
+            { "command": "show gateways-and-servers", "parameters": { "details-level": "full" } },
+            "onReadOnlyCommand");
+    } catch (err) {
+        console.error(err);
+    }
+
+
+
+
+
+}
+
+function showAccessRulebase(accessPolicy, accessLayer) {
+    try {
+        smxProxy.sendRequest("run-readonly-command",
+            {
+                "command": "show access-rulebase", "parameters": {
+                    "details-level": "full",
+                    "uid": accessLayer,
+                    "policy-package": accessPolicy,
+                }
+            },
+            "onShowAccessRulebase");
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function onShowAccessRulebase(val) {
+    log(`\n onShowAccessRulebase ${JSON.stringify(val, null, 2)}`);
 }
 
 function onReadOnlyCommand(val) {
