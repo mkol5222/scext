6ec8868cb9423dad9d67ede6dec712ac4c5ac8b7
Martin Koldodovsky <mkoldov@gmail.com>
2023-09-07 12:36:01 +0200

    .

--- pub/demo1/js/js_interactions.js
+++ pub/demo1/js/js_interactions.js
@@ -4,19 +4,18 @@
 });
 
 function onContext(context) {
-    var tree = JSONTree.create(context);
+    const tree = JSONTree.create(context);
     $("#example").html(tree);
-
-
 }
 
 function handleClick() {
-    smxProxy.sendRequest("run-readonly-command",
-	    {"command" : "show gateways-and-servers","parameters" : {"details-level":"full"}},
-	    "onReadOnlyCommand");
+    alert('hello');
+    // smxProxy.sendRequest("run-readonly-command",
+    //     { "command": "show gateways-and-servers", "parameters": { "details-level": "full" } },
+    //     "onReadOnlyCommand");
 }
 
 function onReadOnlyCommand(val) {
-    var tree2 = JSONTree.create(val);
+    const tree2 = JSONTree.create(val);
     $("#example2").html(tree2);
 }
